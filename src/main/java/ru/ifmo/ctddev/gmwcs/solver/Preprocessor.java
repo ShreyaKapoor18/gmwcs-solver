91bf603351234905ddfb4a440bcdb56a358101b3
Shreya Kapoor <kapoorshreya18@gmail.com>
2020-05-13 21:35:54 +0200

    Trial for executing our own graphs

--- src/main/java/ru/ifmo/ctddev/gmwcs/solver/Preprocessor.java
+++ src/main/java/ru/ifmo/ctddev/gmwcs/solver/Preprocessor.java
@@ -23,7 +23,7 @@ public class Preprocessor {
             }
         }
         for (Node v : new ArrayList<>(graph.vertexSet())) {
-            if (v.getWeight() <= 0 && graph.degreeOf(v) == 2) {
+            //if (v.getWeight() <= 0 && graph.degreeOf(v) == 2) {
                 Edge[] edges = graph.edgesOf(v).stream().toArray(Edge[]::new);
                 if (edges[1].getWeight() > 0 || edges[0].getWeight() > 0) {
                     continue;
@@ -38,7 +38,7 @@ public class Preprocessor {
                     edges[0].absorb(edges[1]);
                     graph.addEdge(left, right, edges[0]);
                 }
-            }
+            //}
         }
     }
 
